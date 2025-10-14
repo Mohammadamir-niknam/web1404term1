0ec3aa03464b84898d3b26df785a4d30c1baae55
Mohammadamir-niknam <kazemniknamm.babaji@gmail.com>
2025-10-14 16:52:57 +0330

    Add Object

--- 01/01h.js
+++ 01/01h.js
@@ -13,6 +13,15 @@ else if(command == "minus")
 {
     console.log(Number(inputs[0]) - Number(inputs[1]))
 }
+else if(command == "print")
+{
+    let obj = {
+        name : inputs[0],
+        family : inputs[1],
+        age : inputs[2]
+    }
+    console.log("Your Object is : " , obj);
+}
 else
 {
     console.log("Please Check Again ")
