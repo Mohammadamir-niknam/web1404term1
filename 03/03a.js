a0e39065ba7ab5248404952b21e865ef66e9f00b
Mohammadamir-niknam <kazemniknamm.babaji@gmail.com>
2025-10-30 08:36:33 +0330

    Update 03a

--- 03/03a.js
+++ 03/03a.js
@@ -35,114 +35,81 @@ else if(command == "print2"){
     }
  
     for(let key in obj){
-        console.log('salam:', obj[key]);
+        console.log('salam', obj[key]);
     }
+
 }
-else if(command == "print3"){
-    let text = 'salam';
+else if (command =="print3"){
+    let text='salam';
     xxx(text);
 }
-else if(command == "print4"){
-    let text = 'salam';
+else if (command =="print4"){
+    let text='salam';
     console.log(yyy(text));
 }
-else if(command === 'write'){
-    let body = yyy('salam');
+else if (command =="write"){
+    let body= yyy('salam');
 
     console.log('a', typeof body, body);
     body = JSON.stringify(body);
-    console.log('b', typeof body, body);
+    console.log("b", typeof body, body);
 
-    console.log(1);
-    fs.writeFile('./data.txt', body, function(err){
-        console.log(2, err);
+    fs.writeFile('./data.txt',body, function(err){
         if(err){
             console.log('file NOT saved.');
         }
         else{
-            console.log('file saved.');
+                console.log('file saved.');
         }
-        console.log('file saved.');
     });
-    console.log(3);
-
 }
-else if(command === 'read'){
-    fs.readFile('./dataddd.txt', {encoding: 'utf8'}, function(err, body){
+else if (command =="read"){
+    fs.readFile('./data.txt', {encoding: 'utf8'}, function(err,body){
         if(err){
-            if(err.code === 'ENOENT'){
-                console.log('File not found');
-            }
-            else{
-                console.log('read error', err);
-            }
+            console.log('read erorr.');
         }
         else{
-            console.log('file opened: ', body);
+                console.log('file opeen: ', body);
         }
     });
 }
-else if(command === 'createrecord'){
-let obj = {
-    name : inputs[0],
-    family : inputs[1],
-    email :inputs[2]
+else if (command =="append"){
+    fs.appendFile('./data.txt',inputs[0],function(err){
+        if(err){
+            console.log('can not append to fill');
+        }
+        else{
+            console.log('append your text to file');
+        }
+    })
 }
-fs.readFile('data.json', {encoding: 'utf8'}, function(err,body){
-    if(err){
-    if(err.code === 'ENOENT'){
-        console.log("File not Found!");
-    }
-    else{
-        console.log("read error",err)
-    }
-    }
-    else{
-        console.log('file open : ',body)
-        body = JSON.parse(body);
-        body.records.push(obj);
-        body = JSON.stringify(body);
-
-        fs.writeFile('./data.json',body,function(err){
-            if(err){
-                console.log('err',err)
-            }
-            else{
-                console.log('Create Success!')
-            }
-        })
-    }
+else {
+    console.log("command not found.")
 }
 
-)}
 
-// else {
-//     console.log("command not found.")
-// }
-
-
-function xxx(asghar){
+function xxx(salam){
     let obj = {
         name: inputs[0],
         family: inputs[1],
         age: inputs[2]
     }
- 
+
     for(let key in obj){
-        console.log(asghar, obj[key]);
+        console.log(salam, obj[key]);
     }
 }
 
-function yyy(asghar){
+function yyy(salam){
     let obj = {
         name: inputs[0],
         family: inputs[1],
         age: inputs[2]
     }
- 
+
     let newObj = {};
     for(let key in obj){
-        newObj[key] = asghar + ' ' + obj[key]
+        newObj[key] = salam + ' ' + obj[key];
     }
     return newObj;
 }
